823784d667ca15423e6bf1e4079f1499f0bd5545
hooperethan123 <emhooper@bsu.edu>
2024-01-21 22:28:10 -0500

    Using the Extract Delegate Refactoring

---
+++ src/ifs/InsuranceCalculator.java
@@ -0,0 +1,19 @@
+package ifs;
+
+public class InsuranceCalculator {
+
+    private final InsuranceStrategyVeryHigh insuranceStrategyVeryHigh = new InsuranceStrategyVeryHigh();
+
+    public double calculateInsurance(double income) {
+        if (income <= 10000) {
+            return income * 0.365;
+        } else if (income <= 30000) {
+            return (income - 10000) * 0.2 + 35600;
+        } else if (income <= 60000) {
+            return (income - 30000) * 0.1 + 76500;
+        } else {
+            return insuranceStrategyVeryHigh.calculateInsuranceVeryHigh(income);
+        }
+    }
+
+}
